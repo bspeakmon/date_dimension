c54bbc8afbfdd13833e1a928bd5d25068fdb8b39
Ben Speakmon <ben.speakmon@gmail.com>
2015-03-04 19:49:32 -0800

    add create statement and allow name/pk customization

--- step_dimension.py
+++ step_dimension.py
@@ -5,13 +5,25 @@ import argparse
 
 class StepDimensionGenerator(object):
 
-    INSERT_TEMPLATE = """INSERT INTO step_dim (step_dim_key, total_steps, this_step, steps_remaining) VALUES ({}, {}, {}, {});"""
+    CREATE_TEMPLATE = """CREATE TABLE {} (
+        {} int,
+        total_steps int,
+        this_step int,
+        steps_remaining int);\n"""
 
-    def __init__(self, max_steps, output_file):
+    INSERT_TEMPLATE = """INSERT INTO step_dim (step_dim_key, total_steps, this_step, steps_remaining) VALUES ({}, {}, {}, {});\n"""
+
+    def __init__(self, max_steps, output_file, table_name, pk_column):
         self.max_steps = max_steps
         self.output_file = output_file
+        self.table_name = table_name
+        self.pk_column = pk_column
 
     def generate(self):
+
+        create_statement = self.CREATE_TEMPLATE.format(self.table_name, self.pk_column)
+        self.output_file.write(create_statement)
+
         row_key = 1
         inserts = []
         for session in range(self.max_steps + 1):
@@ -29,11 +41,20 @@ class StepDimensionGenerator(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('steps', metavar='S', type=int, default=100,
+
+    insert_group = parser.add_argument_group("Insert generation")
+    insert_group.add_argument('-s', '--steps', type=int, default=100,
                         help="Number of steps in the biggest session.")
-    parser.add_argument('-o', '--output-file', type=argparse.FileType('w'),
-                        default='step_dimension_inserts.sql',
-                        help='Output destination for the INSERT script.')
+    insert_group.add_argument('-o', '--output-file', type=argparse.FileType('w'),
+                        default='create_step_dimension.sql',
+                        help='Output destination for the CREATE/INSERT script.')
+
+    create_group = parser.add_argument_group("Table creation")
+    create_group.add_argument('--table-name', default='step_dim',
+                              help='Name of the step dimension table.')
+    create_group.add_argument('--primary-key-column', default="step_dim_key",
+                              help='Namem of the primary key column.')
 
     args = parser.parse_args()
-    StepDimensionGenerator(args.steps, args.output_file).generate()
+    StepDimensionGenerator(args.steps, args.output_file, args.table_name,
+                           args.primary_key_column).generate()
